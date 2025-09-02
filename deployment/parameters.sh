46046a08494e8a13f3dd2b27f5eeb23c7b2ccc14
mfanelo <mfanelom@amazon.com>
2025-09-02 14:21:03 +0200

    added a few files

---
+++ deployment/parameters.sh
@@ -0,0 +1,28 @@
+# Copyright 2022 Amazon Web Services, Inc
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+IDC_LOGIN_URL=https://d-906625c488.awsapps.com/start
+REGION=us-east-1
+TEAM_ACCOUNT=171299924668
+ORG_MASTER_PROFILE=default
+TEAM_ACCOUNT_PROFILE=default
+TEAM_ADMIN_GROUP="Team-admins"
+TEAM_AUDITOR_GROUP="Team-auditors"
+CLOUDTRAIL_AUDIT_LOGS=arn:aws:cloudtrail:us-east-1:171299924668:eventdatastore/b613d01b-71df-4d23-9943-5e5f35450a52
+SECRET_NAME=some-secret
+
+# Uncomment the next line only if you have a custom domain
+# UI_DOMAIN=portal.teamtest.online
+
+
